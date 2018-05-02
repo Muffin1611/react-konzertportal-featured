b9ef1f4dd806c80b8b7b8b6fc2247b66196cf6e6
Muffin <marvin.dick96@web.de>
2018-05-02 02:18:36 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+
+var axios = require('axios');
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = this.getInitialState();
+    }
+
+    getInitialState() {
+        return {
+            concerts: []
+        }
+    }
+
+    componentDidMount() {
+        var th = this;
+        this.serverRequest =
+            axios.get(this.props.source)
+                .then(function(result) {
+                    th.setState({
+                        concerts: result.data.concerts
+                    });
+                })
+    }
+
+    componentWillUnmount() {
+        this.serverRequest.abort();
+    }
+
+    render() {
+        return (
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
+
+
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="ny.jpg" alt="New York"></img>
+                        <div class="carousel-caption">
+                            <h3>Featured</h3>
+                        </div>
+                    </div>
+                    {this.state.concerts.map(function(concert) {
+                        if(concert.featured) {
+                            return (
+                                <div class="item">
+                                    <img src={concert.featuredImage} alt="Something"></img>
+                                    <div class="carousel-caption">
+                                        <h3>{concert.name}</h3>
+                                    </div>
+                                </div>
+                            );}
+                    })}
+                </div>
+
+                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App source={process.env.PUBLIC_URL + '/concerts.json'}/>, document.getElementById('root'));
+registerServiceWorker();
