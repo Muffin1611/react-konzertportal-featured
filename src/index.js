49c686eabec56360b849c3faee680107ebf5f85d
Muffin <marvin.dick96@web.de>
2018-05-03 06:11:50 +0200

    Fixed css for homepage

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App source={process.env.PUBLIC_URL + '/concerts.json'}/>, document.getElementById('root'));
+ReactDOM.render(<App source={process.env.PUBLIC_URL + '/concerts.json'}/>, document.getElementById('featured-root'));
 registerServiceWorker();
